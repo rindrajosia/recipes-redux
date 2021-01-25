525514dba7d9c8ea462ab35efd5d2eafa5f3f91a
rindra josia <rrindrajosia@gmail.com>
2021-01-25 18:40:37 +0300

    Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const CATEGORY_FILTERS = {
+  ALL: 'All',
+};
+
+export const FILTER = [
+  'All',
+  'Side',
+  'Vegetarian',
+  'Dessert',
+  'Pork',
+  'Beef',
+  'Lamb',
+  'Seafood',
+];
